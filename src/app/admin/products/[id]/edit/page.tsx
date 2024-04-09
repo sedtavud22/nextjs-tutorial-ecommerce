38ba7b51983cb663ca7a838959b688eb5abaad44
sedtavud22 <phoosed@gmail.com>
2024-04-09 17:23:02 +0700

    admin product page & admin authentication

---
+++ src/app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,18 @@
+import PageHeader from "@/app/admin/_components/PageHeader";
+import { ProductForm } from "../../_components/ProductForm";
+import prisma from "@/db/db";
+
+export default async function NewProductPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await prisma.product.findUnique({ where: { id } });
+
+  return (
+    <>
+      <PageHeader>Edit Product</PageHeader>
+      <ProductForm product={product} />
+    </>
+  );
+}
